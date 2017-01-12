d93bdbbb6e40197a69a6df7ef1021f77f129d24b
KendrickPC <kennethpchang@gmail.com>
2017-01-12 13:06:17 +0800

    edited the phase variable to reference the scroll variable in the local scope

--- views/js/main.js
+++ views/js/main.js
@@ -505,7 +505,7 @@ function updatePositions() {
   // Declaring the scroll variable below increases fps
   var scroll = (document.body.scrollTop / 1250); 
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin(scroll + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 

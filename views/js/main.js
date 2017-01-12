3e8848f26368bb6ec02d6e0dc25791dc8b0bda18
KendrickPC <kennethpchang@gmail.com>
2017-01-12 13:40:01 +0800

    moved pizzasDiv outside of forloop to decrease load time

--- views/js/main.js
+++ views/js/main.js
@@ -468,8 +468,9 @@ var resizePizzas = function(size) {
 window.performance.mark("mark_start_generating"); // collect timing data
 
 // This for-loop actually creates and appends all of the pizzas when the page loads
+// Moving variable outside loop decreases load time
+var pizzasDiv = document.getElementById("randomPizzas");
 for (var i = 2; i < 100; i++) {
-  var pizzasDiv = document.getElementById("randomPizzas");
   pizzasDiv.appendChild(pizzaElementGenerator(i));
 }
 
